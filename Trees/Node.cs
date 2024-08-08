cd869bd3efb7631f7eed5122c372ff05336e9b5a
mein9 <mateo0221@gmail.com>
2024-08-08 13:41:40 -0700

    update git location

---
+++ Trees/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    public class Node<T>
+    {
+        public T data;
+        public Node<T> left, right;
+
+    }
+}
--- Trees/Program.cs
+++ Trees/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace Trees
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
